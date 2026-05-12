115cb75e22a0e55b908b6c062c91acd901c553ea
jqn554 <joseph.q.nguyen@gmail.com>
2026-05-11 20:11:23 -0500

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import Admin from "./Admin.jsx";
-
-const path = window.location.pathname;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Admin from "./Admin";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {path === "/admin" ? <Admin /> : <App />}
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  </BrowserRouter>
 );
